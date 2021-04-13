ca7bb8bc0ec7e2a4f30e6f1c8c0651d275957cb8
iheppe <iheppe@uw.edu>
2021-04-13 13:36:44 -0700

    added math ops and tuple functions

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -16,6 +16,13 @@ class Calculator {
         return result
     }
     
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        let xValue = lhs.0 + rhs.0
+        let yValue = lhs.1 + rhs.1
+        let point: (Int, Int) = (xValue, yValue)
+        return point
+    }
+    
     func add(lhs: Dictionary<String,Int>, rhs: Dictionary<String,Int>) -> Dictionary<String,Int> {
         let xValue = lhs["x"]! + rhs["x"]!
         let yValue = lhs["y"]! + rhs["y"]!
@@ -25,6 +32,13 @@ class Calculator {
     
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        let xValue = lhs.0 - rhs.0
+        let yValue = lhs.1 - rhs.1
+        let point: (Int, Int) = (xValue, yValue)
+        return point
     }
     
     func subtract(lhs: Dictionary<String,Int>, rhs: Dictionary<String,Int>) -> Dictionary<String,Int> {
@@ -69,6 +83,19 @@ class Calculator {
         }
         return total / args.count
     }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (_ lhs: Int, _ rhs: Int) -> Int) -> Int {
+        let result = op(lhs, rhs)
+        return result
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (_ lhs: Int, _ rhs: Int) -> Int) -> Int {
+        var result = beg
+        for i in args {
+            result = op(result, i)
+        }
+        return result
+    }
 }
 
 let calc = Calculator()  // Don't change this declaration name; it's used in all the tests below
@@ -82,11 +109,11 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-//calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
-//    // This style is one way of writing an anonymous function
-//calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
-//    // This is the second, more terse, style; either works
-//
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
+    // This style is one way of writing an anonymous function
+calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
+    // This is the second, more terse, style; either works
+
 calc.add([1, 2, 3, 4, 5]) == 15
 calc.multiply([1, 2, 3, 4, 5]) == 120
 calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
@@ -94,22 +121,22 @@ calc.count([]) == 0
 calc.avg([2, 2, 2, 2, 2, 2]) == 2
 calc.avg([1, 2, 3, 4, 5]) == 3
 calc.avg([1]) == 1
-//
-//calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
-//    // this is (((0 op 1) op 2) op 3)
-//calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
-//    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
-//calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
-//    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-//
-//let p1 = (5, 5)
-//let p2 = (12, -27)
-//let p3 = (-4, 4)
-//let p4 = (0, 0)
-//calc.add(lhs: p1, rhs: p2) == (17, -22)
-//calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
-//calc.add(lhs: p4, rhs: p4) == (0, 0)
-//calc.add(lhs: p3, rhs: p4) == (-4, 4)
+
+calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
+    // this is (((0 op 1) op 2) op 3)
+calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
+    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
+    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+
+let p1 = (5, 5)
+let p2 = (12, -27)
+let p3 = (-4, 4)
+let p4 = (0, 0)
+calc.add(lhs: p1, rhs: p2) == (17, -22)
+calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
+calc.add(lhs: p4, rhs: p4) == (0, 0)
+calc.add(lhs: p3, rhs: p4) == (-4, 4)
 
 let pd1 = ["x": 5, "y": 5]
 let pd2 = ["x": -4, "y": 4]
